f80aeda62490fe3e495343b283f162c27038c2e0
Nandita Hazra <hazranandita2022@gmail.com>
2022-11-20 15:43:34 +0530

    Modified Java program: ArrayListBulkOperations.java to have different sets of elements in dynamic array.

--- src/main/java/ArrayListBulkOperations.java
+++ src/main/java/ArrayListBulkOperations.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 // Main class.
 public class ArrayListBulkOperations
-{   // Start of main thread.
+{   // Start of main class block.
 
     // Starting point of execution.
     public static void main(String[] args)
@@ -23,48 +23,38 @@ public class ArrayListBulkOperations
 
         /* Adding elements to
          * the string dynamic array in bulk. */
-        Collections.addAll(items, "Storybooks",
-                "Toys",
-                "Puzzles",
-                "Musical-instruments",
-                "Artwork",
-                "Craft-work",
-                "Exercising-equipments",
-                "Articles",
-                "Articles",
-                "Articles");
+        Collections.addAll(items, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#", "#", "#", "#", "#");
 
         /* Printing the newly created
-         * string dynamic array. */
+         * string dynamic array
+         * using for each loop. */
         System.out.println("Newly created string dynamic array-> ");
         for (Object o: items)
         {
-
             System.out.print(o + " "); // Elements are printed space apart.
-
         }
         System.out.println('\n'); // Moving cursor to next 2 lines.
 
-        /* Printing the modified
-         * string dynamic array. */
-
+        /* Modifying elements of
+        * string dynamic array
+        * in bulk.*/
         boolean result
-                = Collections.replaceAll(items, "Articles", "Magazines");
+                = Collections.replaceAll(items, "#", "*");
 
         System.out.println(
-                "Replaced articles with magazines (true/false) ? Answer: "
+                "Replaced # with * (true/false) ? Answer: "
                         + result
         );
 
+        /* Printing the modified
+         * string dynamic array. */
         System.out.println("\nModified string dynamic array-> ");
         for (Object o: items)
         {
-
             System.out.print(o + " "); // Elements are printed space apart.
-
         }
 
 
     }   // End of execution thread.
 
-}   // End of main class.
+}   // End of main class block.
